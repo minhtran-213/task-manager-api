35af018f51d194ada99f86301820ea03d9e006c8
minhtran-213 <minhtran21301@gmail.com>
2022-01-03 22:19:31 +0700

    Revert "add test"
    This reverts commit 24ad65e4e210ea58304a6f7e87c66787009b3cf8.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
-const app = require('./app');
-
+const express = require('express');
+require('./db/mongoose');
 const userRouter = require('./routers/user');
 const taskRouter = require('./routers/task');
 
+const app = express();
 const port = process.env.PORT;
+
+app.use(express.json());
+app.use(userRouter);
+app.use(taskRouter);
 
 app.listen(port, () => {
   console.log('Server is up on port ' + port);
 });
+
+const jwt = require('jsonwebtoken');
+
+const myFunction = async () => {
+  const token = jwt.sign({ _id: 'abc123' }, process.env.JWT_SECRET, {
+    expiresIn: '7 days',
+  });
+  console.log(token);
+
+  const data = jwt.verify(token, process.env.JWT_SECRET);
+  console.log(data);
+};
+
+myFunction();
--- tests/task.test.js
+++
@@ -1,5 +0,0 @@
-const request = require('supertest');
-
-const Task = require('../src/models/task');
-const app = require('../src/app');
-const { userFake, setupDatabase, userFakeId } = require('./fixtures/db');
